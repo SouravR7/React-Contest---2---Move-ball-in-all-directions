27d8f156de2538f0c5835778a230d57b80401472
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-11-04 11:03:51 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+
+const App = () => {
+  const [renderBall, setRenderBall] = useState(false);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [ballPosition,setBallPosition] = useState({
+    left: "0px",
+    top: "0px",
+  });
+  const reset = () => {};
+  const renderChoice = () => {};
+
+  return (
+    <div className="playground">
+      <button onClick={reset} className="reset">
+        Reset
+      </button>
+      {renderChoice()}
+    </div>
+  );
+};
+
+export default App;
